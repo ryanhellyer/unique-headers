a76850a827aae6451246aac039231acb3285accb
Ryan Hellyer <ryanhellyer@gmail.com>
2020-07-31 21:22:38 +0200

    coding standards fixes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name: Unique Headers
 Plugin URI: https://geek.hellyer.kiwi/plugins/unique-headers/
 Description: Unique Headers
@@ -25,13 +25,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-*/
-
+ */
 
 /**
  * Do not continue processing since file was called directly
- * 
+ *
  * @since 1.0
  * @author Ryan Hellyer <ryanhellyer@gmail.com>
  */
@@ -44,27 +42,27 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Autoload the classes.
  * Includes the classes, and automatically instantiates them via spl_autoload_register().
  *
- * @param  string  $class  The class being instantiated
+ * @param string $class The class being instantiated.
  */
 function autoload_unique_headers( $class ) {
 
-	// Bail out if not loading a Media Manager class
-	if ( 'Unique_Headers_' != substr( $class, 0, 15 ) ) {
+	// Bail out if not loading a Media Manager class.
+	if ( 'Unique_Headers_' !== substr( $class, 0, 15 ) ) {
 		return;
 	}
 
-	// Convert from the class name, to the classes file name
+	// Convert from the class name, to the classes file name.
 	$file_data = strtolower( $class );
 	$file_data = str_replace( '_', '-', $file_data );
 	$file_name = 'class-' . $file_data . '.php';
 
-	// Get the classes file path
-	$dir = dirname( __FILE__ );
+	// Get the classes file path.
+	$dir  = dirname( __FILE__ );
 	$path = $dir . '/inc/' . $file_name;
 
-	// Include the class (spl_autoload_register will automatically instantiate it for us)
-	require( $path );
+	// Include the class (spl_autoload_register will automatically instantiate it for us).
+	require $path;
 }
 spl_autoload_register( 'autoload_unique_headers' );
 
-new Unique_Headers_Instantiate;
+new Unique_Headers_Instantiate();
