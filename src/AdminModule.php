eb814a24e86028186add0ecaef1919a776000efb
Ryan <ryanhellyer@gmail.com>
2026-05-01 16:04:24 +0200

    refactor: extract shared image meta box into views/meta-box.php

--- src/AdminModule.php
+++ src/AdminModule.php
@@ -174,26 +174,8 @@ class AdminModule implements ExecutableModule
         $attachmentId = $this->attachmentHelper->getId($post->ID, $this->nameUnderscores);
         $url = $this->attachmentHelper->getSrc($attachmentId);
         $title = $this->attachmentHelper->getTitle($attachmentId);
+        $name = $this->name;
 
-        wp_nonce_field($this->name, $this->name . '-nonce');
-        ?>
-
-        <p class="hide-if-no-js">
-            <a title="<?php echo esc_attr__('Set Custom Header Image', 'unique-headers'); ?>" href="javascript:;" id="<?php echo esc_attr('set-' . $this->name . '-thumbnail'); ?>" class="set-custom-meta-image-thumbnail"><?php echo esc_html__('Set Custom Header Image', 'unique-headers'); ?></a>
-        </p>
-
-        <div id="<?php echo esc_attr($this->name . '-container'); ?>" class="custom-meta-image-container hidden">
-            <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($title); ?>" title="<?php echo esc_attr($title); ?>" />
-        </div>
-
-        <p class="hide-if-no-js hidden">
-            <a title="<?php echo esc_attr__('Remove Custom Header Image', 'unique-headers'); ?>" href="javascript:;" id="<?php echo esc_attr('remove-' . $this->name . '-thumbnail'); ?>" class="remove-custom-meta-image-thumbnail"><?php echo esc_html__('Remove Custom Header Image', 'unique-headers'); ?></a>
-        </p>
-
-        <p id="<?php echo esc_attr($this->name . '-info'); ?>" class="custom-meta-image-info">
-            <input type="hidden" id="<?php echo esc_attr($this->name . '-id'); ?>" class="custom-meta-image-id" name="<?php echo esc_attr($this->name . '-id'); ?>" value="<?php echo esc_attr((string) $attachmentId); ?>" />
-        </p>
-
-        <?php
+        require __DIR__ . '/../views/meta-box.php';
     }
 }
--- src/DisplayModule.php
+++ src/DisplayModule.php
@@ -184,28 +184,14 @@ class DisplayModule implements ExecutableModule
             }
         }
 
-        wp_nonce_field($this->name, $this->name . '-nonce');
+        $name = $this->name;
         ?>
         <tr valign="top">
             <th scope="row"><?php echo esc_html__('Upload header image', 'unique-headers'); ?></th>
             <td>
                 <div id="unique-header" class="postbox">
                     <div class="inside">
-                        <p class="hide-if-no-js">
-                            <a title="<?php echo esc_attr__('Set Custom Header Image', 'unique-headers'); ?>" href="javascript:;" id="<?php echo esc_attr('set-' . $this->name . '-thumbnail'); ?>" class="set-custom-meta-image-thumbnail"><?php echo esc_html__('Set Custom Header Image', 'unique-headers'); ?></a>
-                        </p>
-
-                        <div id="<?php echo esc_attr($this->name . '-container'); ?>" class="custom-meta-image-container hidden">
-                            <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($title); ?>" title="<?php echo esc_attr($title); ?>" />
-                        </div>
-
-                        <p class="hide-if-no-js hidden">
-                            <a title="<?php echo esc_attr__('Remove Custom Header Image', 'unique-headers'); ?>" href="javascript:;" id="<?php echo esc_attr('remove-' . $this->name . '-thumbnail'); ?>" class="remove-custom-meta-image-thumbnail"><?php echo esc_html__('Remove Custom Header Image', 'unique-headers'); ?></a>
-                        </p>
-
-                        <p id="<?php echo esc_attr($this->name . '-info'); ?>" class="custom-meta-image-info">
-                            <input type="hidden" id="<?php echo esc_attr($this->name . '-id'); ?>" class="custom-meta-image-id" name="<?php echo esc_attr($this->name . '-id'); ?>" value="<?php echo esc_attr((string) $attachmentId); ?>" />
-                        </p>
+                        <?php require __DIR__ . '/../views/meta-box.php'; ?>
                     </div>
                 </div>
             </td>
